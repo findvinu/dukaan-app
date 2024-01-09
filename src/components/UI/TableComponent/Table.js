85a6cc70129ba918112e6a4a5be4a1df2275549c
findvinu <vinodyadavhhh4@gmail.com>
2024-01-09 18:55:30 +0530

    add loading and error status

--- src/components/UI/TableComponent/Table.js
+++ src/components/UI/TableComponent/Table.js
@@ -1,38 +1,53 @@
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-import tableData from "../../../api/table_data.json";
+import { useEffect, useState } from "react";
+import axios from "axios";
+// import tableData from "../../../api/table_data.json";
 import classes from "./Table.module.scss";
 import DropIcon from "../../../assets/svg-icons/drop-icon";
 import Info from "../../../assets/images/info.png";
 
 const Table = () => {
-  /* const [tableData, setTableData] = useState([]);
+  const [tableData, setTableData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   useEffect(() => {
     const getDataFromApi = async () => {
       try {
         const response = await axios.get("/api/table_data.json");
         setTableData(response.data);
-        console.log(response.data);
+        setIsLoading(false);
       } catch (error) {
-        console.error("Error sending data:", error);
+        setError(error);
+        setIsLoading(false);
       }
     };
+
     getDataFromApi();
-  }, []); */
+  }, []);
+
+  if (isLoading) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="error">Error: {error.message}</div>;
+  }
 
   return (
     <div className={classes.tableWrapperBlock}>
       <table>
         <thead>
-          <th className={classes.orderIdHead}>Order ID</th>
-          <th className={classes.orderDateHead}>
-            Order date <DropIcon width="7px" height="5px" />
-          </th>
-          <th className={classes.orderAmountHead}>Order amount</th>
-          <th className={classes.orderFeesHead}>
-            Transaction fees{" "}
-            <img src={Info} alt="info" title="Transaction fees" />{" "}
-          </th>
+          <tr>
+            <th className={classes.orderIdHead}>Order ID</th>
+            <th className={classes.orderDateHead}>
+              Order date <DropIcon width="7px" height="5px" />
+            </th>
+            <th className={classes.orderAmountHead}>Order amount</th>
+            <th className={classes.orderFeesHead}>
+              Transaction fees{" "}
+              <img src={Info} alt="info" title="Transaction fees" />{" "}
+            </th>
+          </tr>
         </thead>
         <tbody>
           {tableData.map((tableItem) => {
